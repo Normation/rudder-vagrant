ff5bcdec1dbcf8beb49c94eb860c60a047ffe286
Vincent Membré <vincent.membre@normation.com>
2013-04-24 11:17:12 +0200

    Create 2.6 branch

--- provision/server.sh
+++ provision/server.sh
@@ -22,8 +22,7 @@
 # Fetch parameters
 KEYSERVER=keyserver.ubuntu.com
 KEY=474A19E8
-RUDDER_REPO_URL="http://www.rudder-project.org/apt-2.5/"
-RUDDER_REPO_URL2.6="http://www.rudder-project.org/apt-2.6/"
+RUDDER_REPO_URL="http://www.rudder-project.org/apt-2.6/"
 RUDDER_REPO_URL2.6_NIGHTLY="http://www.rudder-project.org/apt-2.6-nightly/"
 
 # Rudder related parameters
@@ -79,7 +78,6 @@ echo "deb http://ftp.fr.debian.org/debian/ ${DEBIAN_RELEASE}-updates main" >> /e
 echo "deb-src http://ftp.fr.debian.org/debian/ ${DEBIAN_RELEASE}-updates main" >> /etc/apt/sources.list
 
 echo "deb ${RUDDER_REPO_URL} ${DEBIAN_RELEASE} main contrib non-free" > /etc/apt/sources.list.d/rudder.list
-echo "#deb ${RUDDER_REPO_URL2.6} ${DEBIAN_RELEASE} main contrib non-free" > /etc/apt/sources.list.d/rudder.list
 echo "#deb ${RUDDER_REPO_URL2.6_NIGHTLY} ${DEBIAN_RELEASE} main contrib non-free" > /etc/apt/sources.list.d/rudder.list
 
 # Update APT cache
