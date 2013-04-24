b1acec967fd94a0fd4319c0d2e2d6c3a6293fd62
Vincent Membré <vincent.membre@normation.com>
2013-04-24 12:22:17 +0200

    Add 2.6 repository for node

--- provision/node_sles11.sh
+++ provision/node_sles11.sh
@@ -23,10 +23,14 @@
 # Rudder version
 RUDDER_VERSION="2.4"
 RUDDER_VERSION25="2.5"
+RUDDER_VERSION26="2.6"
+RUDDER_VERSION26_NIGHTLY="2.6"
 
 # Fetch parameters
 RUDDER_REPO_URL="http://www.rudder-project.org/rpm-2.4/SLES_11_SP1/"
 RUDDER_REPO_URL25="http://www.rudder-project.org/rpm-2.5/SLES_11_SP1/"
+RUDDER_REPO_URL26="http://www.rudder-project.org/rpm-2.6/SLES_11_SP1/"
+RUDDER_REPO_URL26_NIGHTLY="http://www.rudder-project.org/rpm-2.6-nightly/SLES_11_SP1/"
 
 ZYPPER_ARGS="--non-interactive --no-gpg-checks"
 
@@ -60,6 +64,28 @@ name=Rudder ${RUDDER_VERSION25} Nightly RPM
 enabled=0
 autorefresh=0
 baseurl=${RUDDER_REPO_URL25}
+type=rpm-md
+keeppackages=0
+EOF
+
+# Add Rudder 2.6 repository
+cat > /etc/zypp/repos.d/Rudder2.6.repo <<EOF
+[Rudder${RUDDER_VERSION26}Nightly]
+name=Rudder ${RUDDER_VERSION26} Nightly RPM
+enabled=0
+autorefresh=0
+baseurl=${RUDDER_REPO_URL26}
+type=rpm-md
+keeppackages=0
+EOF
+
+# Add Rudder repository
+cat > /etc/zypp/repos.d/Rudder2.6-nightly.repo <<EOF
+[Rudder${RUDDER_VERSION26_NIGHTLY}Nightly]
+name=Rudder ${RUDDER_VERSION26_NIGHTLY} Nightly RPM
+enabled=0
+autorefresh=0
+baseurl=${RUDDER_REPO_URL26_NIGHTLY}
 type=rpm-md
 keeppackages=0
 EOF
