ad7ff5b1cda8c6d17e89c3db071a863d8b532dc3
Vincent Membré <vincent.membre@normation.com>
2013-06-03 17:42:09 +0200

    Fixes for SLES_11²

--- provision/server_sles11.sh
+++ provision/server_sles11.sh
@@ -24,8 +24,8 @@ RUDDER_VERSION="2.6"
 RUDDER_VERSION26_NIGHTLY="2.6-nightly"
 
 # Fetch parameters
-RUDDER_REPO_URL="http://www.rudder-project.org/rpm-2.6/SLES_11/"
-RUDDER_REPO_URL26_NIGHTLY="http://www.rudder-project.org/rpm-2.6-nightly/SLES_11/"
+RUDDER_REPO_URL="http://www.rudder-project.org/rpm-2.6/SLES_11_SP1/"
+RUDDER_REPO_URL26_NIGHTLY="http://www.rudder-project.org/rpm-2.6-nightly/SLES_11_SP1/"
 
 ZYPPER_ARGS="--non-interactive --no-gpg-checks"
 
