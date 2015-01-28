d33df0da5c74cfb53380e8ad1f27f34d4cd906d6
Nicolas CHARLES <nicolas.charles@normation.com>
2015-01-28 14:17:24 +0100

    Fixes #6195: correct centos6 node provisioner

--- provision/node_centos6.sh
+++ provision/node_centos6.sh
@@ -37,6 +37,7 @@ echo -e "\n192.168.42.10	server.rudder.local" >> /etc/hosts
 
 # Add Rudder repositories
 for RUDDER_VERSION in 2.10 2.11 3.0
+do
     if [ "${RUDDER_VERSION}" == "2.10" ]; then
         ENABLED=1
     else
